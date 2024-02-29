ca8f1c013745c05917e1a075242413de285a1289
gourisouji <soujanyagourishetty1999@gmail.com>
2024-02-29 15:48:13 +0530

    first commit

---
+++ src/main/java/com/telusko/quizapp/model/Question.java
@@ -0,0 +1,27 @@
+package com.telusko.quizapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Question {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id;
+	private String questionTitle;
+	private String option1;
+	private String option2;
+	private String option3;
+	private String option4;
+	private String rightAnswer;
+	private String difficultyLevel;
+	private String category;
+
+}
